133d59079352ac718fb1c0c2e1522da35192e89c
Olga Mishareva <mishareva.olga@gmail.com>
2023-05-11 19:37:36 +0200

    feat: add mocke chessboard-cages

---
+++ src/components/constants/chessboard-cages.js
@@ -0,0 +1,20 @@
+const chessboardCages = [
+  "a8",
+  "b8",
+  "c8",
+  "d8",
+  "e8",
+  "g8",
+  "f8",
+  "h8",
+  "a7",
+  "b7",
+  "c7",
+  "d7",
+  "e7",
+  "g7",
+  "f7",
+  "h7",
+];
+
+export default chessboardCages;
