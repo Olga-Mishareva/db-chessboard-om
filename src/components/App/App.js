09c978c78fd7664f7b8006d070f521b31e010c20
Olga Mishareva <mishareva.olga@gmail.com>
2023-05-14 17:57:23 +0200

    feat: add no-match component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,17 @@ import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Chessboard from "../Chessboard/Chessboard";
+import NoMatch from "../NoMatch/NoMatch";
 
 function App() {
   return (
     <div className="page">
       <header className="header"></header>
       <Routes>
+        <Route path="/" element={<NoMatch />} />
         <Route path="/chessboard" element={<Chessboard />} />
-        <Route path="/chessboard?start=" element={<Chessboard />} />
-        <Route path="*" element={<p>404</p>}></Route>
+        <Route path="/chessboard?start" element={<Chessboard />} />
+        <Route path="*" element={<NoMatch />}></Route>
       </Routes>
       <footer></footer>
     </div>
