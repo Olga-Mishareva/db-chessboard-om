701c49adf8f2cf0c9e046754a5f07e7b6f0d3ba3
Olga Mishareva <mishareva.olga@gmail.com>
2023-05-15 11:52:38 +0200

    feat: add header & footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,14 +7,16 @@ import NoMatch from "../NoMatch/NoMatch";
 function App() {
   return (
     <div className="page">
-      <header className="header"></header>
+      <header className="header">
+        <h1>Mögliche Züge eines Springers</h1>
+      </header>
       <Routes>
         <Route path="/" element={<NoMatch />} />
         <Route path="/chessboard" element={<Chessboard />} />
         <Route path="/chessboard?start" element={<Chessboard />} />
         <Route path="*" element={<NoMatch />}></Route>
       </Routes>
-      <footer></footer>
+      <footer className="footer">OM</footer>
     </div>
   );
 }
